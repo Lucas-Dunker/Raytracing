93992cabd12f24fca0ddd910089a4bb6121461c4
Lucas Dunker <lucasmd.dev@gmail.com>
2026-02-02 21:33:06 -0500

    Chapter 2

--- main.cc
+++ main.cc
@@ -1,16 +1,21 @@
 #include <iostream>
 
-int main() {
+int main()
+{
     // Image
 
     int image_width = 256;
     int image_height = 256;
 
     // Render
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::cout << "P3\n"
+              << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = 0; j < image_height; j++) {
-        for (int i = 0; i < image_width; i++) {
+    for (int j = 0; j < image_height; j++)
+    {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; i++)
+        {
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
             auto b = 0.0;
@@ -19,7 +24,9 @@ int main() {
             int ig = int(255.999 * g);
             int ib = int(255.999 * b);
 
-            std::cout << ir << ' ' << ig << " " << ib << "\n";
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+
+    std::clog << "\rDone.                 \n";
 }
